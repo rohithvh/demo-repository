436ee051db35596a7210016484795876a9833bb2
rohithvh <103938529+rohithvh@users.noreply.github.com>
2025-09-18 05:25:20 +0000

    Update main.tf

--- examples/main.tf
+++ examples/main.tf
@@ -1,6 +1,10 @@
 module "s3_depoly" {
-  source  = ""
+  source  = "app.terraform.io/Rohith_Demo/s3_bucket/aws"
   version = "1.0.0"
   bucket = var.bucket
   tags   = var.tags
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
