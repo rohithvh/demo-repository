1540e3d64e8111555ee5520fb928c0d1ee2584b8
rohithvh <103938529+rohithvh@users.noreply.github.com>
2025-09-30 09:53:00 +0000

    Update S3 bucket outputs to use 'this' reference

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 output "bucket_arn" {
-value = aws_s3_bucket.test_bucket.arn
+value = aws_s3_bucket.this.arn
 description= "arn of s3 bucket"
 }
 
 
 output "bucket_name" {
-value = aws_s3_bucket.test_bucket.id
+value = aws_s3_bucket.this.id
 description= "name of s3 bucket"
 }
